465d80ed9510592327f0883108ffff0eb174cd74
GenZay <indan.gumelar20@gmail.com>
2018-03-02 19:27:12 +0700

    Add project weather

---
+++ app/src/main/java/com/atasin/weather/Model/Clouds.java
@@ -0,0 +1,21 @@
+package com.atasin.weather.Model;
+
+/**
+ * Created by Akhdan Rasiq on 12/02/2018.
+ */
+
+public class Clouds {
+    private int all;
+
+    public Clouds(int all) {
+        this.all = all;
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
---
+++ app/src/main/java/com/atasin/weather/Model/Rain.java
@@ -0,0 +1,8 @@
+package com.atasin.weather.Model;
+
+/**
+ * Created by Akhdan Rasiq on 12/02/2018.
+ */
+
+public class Rain {
+}
